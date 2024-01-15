b53f03728ef36325bb1ac0437d0e5eae3de9caa9
CristianRanieri <C.RANIERI7@STUDENTI.UNISA.IT>
2024-01-15 21:40:41 +0100

    Fix error accessi effettua e visualizza ordine.

--- src/main/java/GestioneOrdini/Control/EffettuaOrdine.java
+++ src/main/java/GestioneOrdini/Control/EffettuaOrdine.java
@@ -43,89 +43,86 @@ public class EffettuaOrdine extends HttpServlet {
             //l'attore non ha i permessi per effettuare un ordine
             RequestDispatcher dispatcher= req.getRequestDispatcher("/WEB-INF/errorePermessi.jsp");
             dispatcher.forward(req,resp);
-        }else {
-            if(account.getId() == -1){
-                resp.sendRedirect("visualizzaLogin");
-            }else {
-                if(account.getCarrello().isEmpty()){
-                    resp.sendRedirect("visualizzaCarrello");
-                }else {
-                    //Controllo gli input
-                    if(req.getParameter("nome")!= null && req.getParameter("cognome")!= null && req.getParameter("carta")!= null && !req.getParameter("dataScadenza").isEmpty()
-                            && req.getParameter("cvv")!= null && req.getParameter("nomeCarta")!= null && req.getParameter("indirizzo")!= null
-                            && req.getParameter("citta")!= null && req.getParameter("cap")!= null && req.getParameter("provincia")!= null
-                            && PatternInput.nome(req.getParameter("nome")) && PatternInput.nome(req.getParameter("cognome"))
-                            && PatternInput.numeroCarta(req.getParameter("carta")) && PatternInput.data(new Date(Integer.parseInt(req.getParameter("dataScadenza").substring(0,4))-1900,Integer.parseInt(req.getParameter("dataScadenza").substring(5,7))-1,1))
-                            && PatternInput.numeroCCV(req.getParameter("cvv")) && PatternInput.nomeCognome(req.getParameter("nomeCarta"))
-                            && PatternInput.stringaConSpazzi(req.getParameter("indirizzo")) && PatternInput.stringCaratteriSpeciali(req.getParameter("citta"))
-                            && PatternInput.numeroCAP(req.getParameter("cap")) && PatternInput.stringCaratteriSpeciali(req.getParameter("provincia"))
-                    ){
-                        //controllo validita carrello
-                        if(ValidaCarrello.validazioneCarrello(account.getCarrello())) {
+        } else {
 
-                            //Gli input sono validi
-                            Ordine ordine = new Ordine();
-                            ordine.setAccount(account);
-                            ordine.setCAP(req.getParameter("cap"));
-                            Date date = new Date(System.currentTimeMillis());
-                            LocalDate localDate = date.toLocalDate();
-                            ordine.setData(Date.valueOf(localDate));
-                            ordine.setCitta(req.getParameter("citta"));
-                            ordine.setIndirizzo(req.getParameter("indirizzo"));
-                            ordine.setProvincia(req.getParameter("provincia"));
+            if(account.getCarrello().isEmpty()) {
+                resp.sendRedirect("visualizzaCarrello");
+            } else {
+                //Controllo gli input
+                if (req.getParameter("nome") != null && req.getParameter("cognome") != null && req.getParameter("carta") != null && !req.getParameter("dataScadenza").isEmpty()
+                        && req.getParameter("cvv") != null && req.getParameter("nomeCarta") != null && req.getParameter("indirizzo") != null
+                        && req.getParameter("citta") != null && req.getParameter("cap") != null && req.getParameter("provincia") != null
+                        && PatternInput.nome(req.getParameter("nome")) && PatternInput.nome(req.getParameter("cognome"))
+                        && PatternInput.numeroCarta(req.getParameter("carta")) && PatternInput.data(new Date(Integer.parseInt(req.getParameter("dataScadenza").substring(0, 4)) - 1900, Integer.parseInt(req.getParameter("dataScadenza").substring(5, 7)) - 1, 1))
+                        && PatternInput.numeroCCV(req.getParameter("cvv")) && PatternInput.nomeCognome(req.getParameter("nomeCarta"))
+                        && PatternInput.stringaConSpazzi(req.getParameter("indirizzo")) && PatternInput.stringCaratteriSpeciali(req.getParameter("citta"))
+                        && PatternInput.numeroCAP(req.getParameter("cap")) && PatternInput.stringCaratteriSpeciali(req.getParameter("provincia"))
+                ) {
+                    //controllo validita carrello
+                    if (ValidaCarrello.validazioneCarrello(account.getCarrello())) {
 
-                            double prezzoTotale = 0;
-                            ArrayList<AcquistoProdotto> prodotti = new ArrayList<>();
+                        //Gli input sono validi
+                        Ordine ordine = new Ordine();
+                        ordine.setAccount(account);
+                        ordine.setCAP(req.getParameter("cap"));
+                        Date date = new Date(System.currentTimeMillis());
+                        LocalDate localDate = date.toLocalDate();
+                        ordine.setData(Date.valueOf(localDate));
+                        ordine.setCitta(req.getParameter("citta"));
+                        ordine.setIndirizzo(req.getParameter("indirizzo"));
+                        ordine.setProvincia(req.getParameter("provincia"));
 
-                            for (ContenutoCarrello cc : account.getCarrello().getContenutoCarrello()) {
-                                AcquistoProdotto acquistoProdotto = new AcquistoProdotto();
-                                acquistoProdotto.setProdotto(cc.getProdotto());
-                                acquistoProdotto.setQuantita(cc.getQuantita());
-                                acquistoProdotto.setPrezzoAcquisto(cc.getProdotto().getPrezzo());
-                                prodotti.add(acquistoProdotto);
-                                prezzoTotale += cc.getProdotto().getPrezzo() * cc.getQuantita();
-                            }
-                            ordine.setPrezzoTotale(BigDecimal.valueOf(prezzoTotale).setScale(2, RoundingMode.HALF_UP).doubleValue());
-                            ordine.setProdotti(prodotti);
+                        double prezzoTotale = 0;
+                        ArrayList<AcquistoProdotto> prodotti = new ArrayList<>();
 
-                            GestioneOrdiniService gestioneOrdiniService = new GestioneOrdiniService();
-                            gestioneOrdiniService.effettuaOrdine(ordine);
-
-                            Pagamento pagamento = new Pagamento();
-                            pagamento.setData(String.valueOf(new Date(Integer.parseInt(req.getParameter("dataScadenza").substring(0, 4)) - 1900, Integer.parseInt(req.getParameter("dataScadenza").substring(5, 7)) - 1, 1)));
-                            pagamento.setCvv(req.getParameter("cvv"));
-                            pagamento.setValorePagamento(prezzoTotale);
-                            pagamento.setNumeroCarta(req.getParameter("carta"));
-
-                            PagamentoService pagamentoService = new PagamentoAdapter();
-                            boolean b = pagamentoService.pagamento(pagamento);
-
-                            if (b) {
-                                Carrello carrello = new Carrello();
-                                carrello.setContenutoCarrello(new ArrayList<ContenutoCarrello>());
-
-                                account.setCarrello(carrello);
-
-                                req.setAttribute("Successo", true);
-                                RequestDispatcher dispatcher = req.getRequestDispatcher("visualizzaOrdini");
-                                dispatcher.forward(req, resp);
-                            } else {
-                                req.setAttribute("Pagamento-Fallito", false);
-                                RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/carrello.jsp");
-                                dispatcher.forward(req, resp);
-                            }
-                        }else {
-                            //carrello non valido, setto l'ttributo per notificarlo all'utente
-                            req.setAttribute("cambiamenti",true);
-                            RequestDispatcher dispatcher= req.getRequestDispatcher("visualizzaCarrello");
-                            dispatcher.forward(req,resp);
+                        for (ContenutoCarrello cc : account.getCarrello().getContenutoCarrello()) {
+                            AcquistoProdotto acquistoProdotto = new AcquistoProdotto();
+                            acquistoProdotto.setProdotto(cc.getProdotto());
+                            acquistoProdotto.setQuantita(cc.getQuantita());
+                            acquistoProdotto.setPrezzoAcquisto(cc.getProdotto().getPrezzo());
+                            prodotti.add(acquistoProdotto);
+                            prezzoTotale += cc.getProdotto().getPrezzo() * cc.getQuantita();
                         }
-                    }else {
-                        //Gli input non sono validi
-                        req.setAttribute("errore", true);
-                        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/effettuaOrdine.jsp");
+                        ordine.setPrezzoTotale(BigDecimal.valueOf(prezzoTotale).setScale(2, RoundingMode.HALF_UP).doubleValue());
+                        ordine.setProdotti(prodotti);
+
+                        GestioneOrdiniService gestioneOrdiniService = new GestioneOrdiniService();
+                        gestioneOrdiniService.effettuaOrdine(ordine);
+
+                        Pagamento pagamento = new Pagamento();
+                        pagamento.setData(String.valueOf(new Date(Integer.parseInt(req.getParameter("dataScadenza").substring(0, 4)) - 1900, Integer.parseInt(req.getParameter("dataScadenza").substring(5, 7)) - 1, 1)));
+                        pagamento.setCvv(req.getParameter("cvv"));
+                        pagamento.setValorePagamento(prezzoTotale);
+                        pagamento.setNumeroCarta(req.getParameter("carta"));
+
+                        PagamentoService pagamentoService = new PagamentoAdapter();
+                        boolean b = pagamentoService.pagamento(pagamento);
+
+                        if (b) {
+                            Carrello carrello = new Carrello();
+                            carrello.setContenutoCarrello(new ArrayList<ContenutoCarrello>());
+
+                            account.setCarrello(carrello);
+
+                            req.setAttribute("Successo", true);
+                            RequestDispatcher dispatcher = req.getRequestDispatcher("visualizzaOrdini");
+                            dispatcher.forward(req, resp);
+                        } else {
+                            req.setAttribute("Pagamento-Fallito", false);
+                            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/carrello.jsp");
+                            dispatcher.forward(req, resp);
+                        }
+                    } else {
+                        //carrello non valido, setto l'ttributo per notificarlo all'utente
+                        req.setAttribute("cambiamenti", true);
+                        RequestDispatcher dispatcher = req.getRequestDispatcher("visualizzaCarrello");
                         dispatcher.forward(req, resp);
                     }
+                } else {
+                    //Gli input non sono validi
+                    req.setAttribute("errore", true);
+                    RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/effettuaOrdine.jsp");
+                    dispatcher.forward(req, resp);
                 }
             }
         }
